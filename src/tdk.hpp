0f8e9d24926333381119e29477cfe61cbc706882
Sherman Rofeman <skippyr.developer@gmail.com>
2024-05-23 16:17:28 -0300

    update readKeyEvent declaration

--- src/tdk.hpp
+++ src/tdk.hpp
@@ -2,186 +2,217 @@
 
 #include <iostream>
 
-namespace tdk {
-enum class Shape {
-  Default,
-  BlinkingBlock,
-  Block,
-  BlinkingUnderline,
-  Underline,
-  BlinkingBar,
-  Bar
-};
+namespace tdk
+{
+    enum class Shape
+    {
+        Default,
+        BlinkingBlock,
+        Block,
+        BlinkingUnderline,
+        Underline,
+        BlinkingBar,
+        Bar
+    };
 
-enum class Effect {
-  Italic = 3,
-  Underline,
-  Blinking,
-  Negative = 7,
-  Hidden,
-  Strikethrough
-};
+    enum class Effect
+    {
+        Italic = 3,
+        Underline,
+        Blinking,
+        Negative = 7,
+        Hidden,
+        Strikethrough
+    };
 
-enum class Key {
+    enum class Key
+    {
 #ifdef _WIN32
-  LeftArrow = -23,
-  UpArrow,
-  RightArrow,
-  DownArrow,
+        LeftArrow = -23,
+        UpArrow,
+        RightArrow,
+        DownArrow,
 #else
-  UpArrow = -23,
-  DownArrow,
-  RightArrow,
-  LeftArrow,
+        UpArrow = -23,
+        DownArrow,
+        RightArrow,
+        LeftArrow,
 #endif
-  F1,
-  F2,
-  F3,
-  F4,
-  F5,
-  F6,
-  F7,
-  F8,
-  F9,
-  F10,
-  F11,
-  F12,
+        F1,
+        F2,
+        F3,
+        F4,
+        F5,
+        F6,
+        F7,
+        F8,
+        F9,
+        F10,
+        F11,
+        F12,
 #ifdef _WIN32
-  PageUp,
-  PageDown,
-  End,
-  Home,
-  Insert,
-  Delete,
+        PageUp,
+        PageDown,
+        End,
+        Home,
+        Insert,
+        Delete,
 #else
-  Home,
-  Insert,
-  Delete,
-  End,
-  PageUp,
-  PageDown,
+        Home,
+        Insert,
+        Delete,
+        End,
+        PageUp,
+        PageDown,
 #endif
-  Tab = 9,
+        Tab = 9,
 #if defined(_WIN32) || defined(__APPLE__)
-  Enter = 13,
+        Enter = 13,
 #else
-  Enter,
+        Enter,
 #endif
-  Escape = 27,
-  Spacebar = 32,
-  Backspace = 127
-};
+        Escape = 27,
+        Spacebar = 32,
+        Backspace = 127
+    };
 
-enum class EventStatus { Success, WindowResizeInterrupt = -2, Failure };
+    enum class EventStatus
+    {
+        Success,
+        NoEvent = -4,
+        TimeOut,
+        WindowsResize,
+        Failure
+    };
 
-enum class Layer { Foreground = 3, Background };
+    enum class Layer
+    {
+        Foreground = 3,
+        Background
+    };
 
-enum class Stream { Input, Output, Error };
+    enum class Stream
+    {
+        Input,
+        Output,
+        Error
+    };
 
-enum class XColorCode {
-  Default = -1,
-  Black,
-  Red,
-  Green,
-  Yellow,
-  Blue,
-  Magenta,
-  Cyan,
-  White,
-  LightBlack,
-  LightRed,
-  LightGreen,
-  LightYellow,
-  LightBlue,
-  LightMagenta,
-  LightCyan,
-  LightWhite
-};
+    enum class XColorCode
+    {
+        Default = -1,
+        Black,
+        Red,
+        Green,
+        Yellow,
+        Blue,
+        Magenta,
+        Cyan,
+        White,
+        LightBlack,
+        LightRed,
+        LightGreen,
+        LightYellow,
+        LightBlue,
+        LightMagenta,
+        LightCyan,
+        LightWhite
+    };
 
-enum class Weight { Default, Bold, Dim };
+    enum class Weight
+    {
+        Default,
+        Bold,
+        Dim
+    };
 
-class Coordinate {
-public:
-  Coordinate();
-  Coordinate(unsigned short column, unsigned short row);
+    class Coordinate
+    {
+    public:
+        Coordinate();
+        Coordinate(unsigned short column, unsigned short row);
 
-  unsigned short m_column;
-  unsigned short m_row;
-};
+        unsigned short m_column;
+        unsigned short m_row;
+    };
 
-class Dimensions {
-public:
-  Dimensions();
-  Dimensions(unsigned short totalColumns, unsigned short totalRows);
+    class Dimensions
+    {
+    public:
+        Dimensions();
+        Dimensions(unsigned short totalColumns, unsigned short totalRows);
 
-  unsigned short m_totalColumns;
-  unsigned short m_totalRows;
-};
+        unsigned short m_totalColumns;
+        unsigned short m_totalRows;
+    };
 
-class HexColor {
-public:
-  HexColor(unsigned int code, Layer layer);
-  HexColor m_invert();
+    class HexColor
+    {
+    public:
+        HexColor(unsigned int code, Layer layer);
+        HexColor m_invert();
 
-  unsigned int m_code;
-  Layer m_layer;
-};
+        unsigned int m_code;
+        Layer m_layer;
+    };
 
-class KeyEvent {
-public:
-  KeyEvent();
+    class KeyEvent
+    {
+    public:
+        KeyEvent();
 
-  int m_key;
-  bool m_hasAlt;
-  bool m_hasCtrl;
-};
+        int m_key;
+        bool m_hasAlt;
+        bool m_hasCtrl;
+    };
 
-class RGBColor {
-public:
-  RGBColor(unsigned char red, unsigned char green, unsigned char blue,
-           Layer layer);
-  RGBColor(HexColor color);
-  RGBColor m_invert();
+    class RGBColor
+    {
+    public:
+        RGBColor(unsigned char red, unsigned char green, unsigned char blue, Layer layer);
+        RGBColor(HexColor color);
+        RGBColor m_invert();
 
-  unsigned char m_red;
-  unsigned char m_green;
-  unsigned char m_blue;
-  Layer m_layer;
-};
+        unsigned char m_red;
+        unsigned char m_green;
+        unsigned char m_blue;
+        Layer m_layer;
+    };
 
-class XColor {
-public:
-  XColor(unsigned char code, Layer layer);
-  XColor(XColorCode code, Layer layer);
-  XColor m_invert();
+    class XColor
+    {
+    public:
+        XColor(unsigned char code, Layer layer);
+        XColor(XColorCode code, Layer layer);
+        XColor m_invert();
 
-  int m_code;
-  Layer m_layer;
-};
+        int m_code;
+        Layer m_layer;
+    };
 
-std::ostream &operator<<(std::ostream &stream, Effect effect);
-std::ostream &operator>>(std::ostream &stream, Effect effect);
-std::ostream &operator<<(std::ostream &stream, HexColor color);
-std::ostream &operator<<(std::ostream &stream, RGBColor color);
-std::ostream &operator<<(std::ostream &stream, XColor color);
-std::ostream &operator<<(std::ostream &stream, Weight weight);
-bool operator==(int code, Key key);
-bool operator!=(int code, Key key);
-bool operator>(int code, Key key);
-bool operator>=(int code, Key key);
-bool operator<(int code, Key key);
-bool operator<=(int code, Key key);
+    std::ostream& operator<<(std::ostream& stream, Effect effect);
+    std::ostream& operator>>(std::ostream& stream, Effect effect);
+    std::ostream& operator<<(std::ostream& stream, HexColor color);
+    std::ostream& operator<<(std::ostream& stream, RGBColor color);
+    std::ostream& operator<<(std::ostream& stream, XColor color);
+    std::ostream& operator<<(std::ostream& stream, Weight weight);
+    bool operator==(int code, Key key);
+    bool operator!=(int code, Key key);
+    bool operator>(int code, Key key);
+    bool operator>=(int code, Key key);
+    bool operator<(int code, Key key);
+    bool operator<=(int code, Key key);
 
-void clearCursorLine();
-void clearInputBuffer();
-int getCursorCoordinate(Coordinate &coordinate);
-int getWindowDimensions(Dimensions &dimensions);
-bool isTTY(Stream stream);
-EventStatus readKeyEvent(KeyEvent &event);
-void ringBell();
-void setAlternateWindow(bool isToOpen);
-void setCursorCoordinate(unsigned short column, unsigned short row);
-void setCursorCoordinate(Coordinate &coordinate);
-void setCursorShape(Shape shape);
-void setCursorVisibility(bool isToShow);
-} // namespace tdk
+    void clearCursorLine();
+    void clearInputBuffer();
+    int getCursorCoordinate(Coordinate& coordinate);
+    int getWindowDimensions(Dimensions& dimensions);
+    bool isTTY(Stream stream);
+    EventStatus readKeyEvent(KeyEvent& event, int waitInMilliseconds);
+    void ringBell();
+    void setAlternateWindow(bool isToOpen);
+    void setCursorCoordinate(unsigned short column, unsigned short row);
+    void setCursorCoordinate(Coordinate& coordinate);
+    void setCursorShape(Shape shape);
+    void setCursorVisibility(bool isToShow);
+}
