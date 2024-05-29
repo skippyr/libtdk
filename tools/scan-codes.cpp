2ad0898e533a8fe8ec9975a869a6d95615eb38bd
Sherman Rofeman <skippyr.developer@gmail.com>
2024-05-29 15:50:42 -0300

    add colors to tool

--- tools/scan-codes.cpp
+++ tools/scan-codes.cpp
@@ -5,15 +5,19 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define BOOLEAN_STATE(a_value) (a_value ? "\x1b[32menabled\x1b[39m" : "\x1b[31mdisabled\x1b[39m")
+
 static void writeHelpPage()
 {
-    std::cout << "Usage: scan-codes [OPTION]..." << std::endl
+    std::cout << "\x1b[1mUsage:\x1b[22m \x1b[32mscan-codes \x1b[39m[\x1b[33mOPTION\x1b[39m]..."
+              << std::endl
               << "Writes the scan codes related to key, focus and mouse events." << std::endl
               << "By default, it only handle key events." << std::endl
               << std::endl
-              << "OPTIONS" << std::endl
-              << "    --allow-mouse  allow mouse events." << std::endl
-              << "    --allow-focus  allow focus events." << std::endl;
+              << "\x1b[1mOPTIONS\x1b[22m" << std::endl
+              << "           \x1b[33m--help\x1b[39m  shows these help instructions." << std::endl
+              << "    \x1b[33m--allow-mouse\x1b[39m  allows mouse events." << std::endl
+              << "    \x1b[33m--allow-focus\x1b[39m  allows focus events." << std::endl;
 }
 
 int main(int totalArguments, const char** arguments)
@@ -53,27 +57,18 @@ int main(int totalArguments, const char** arguments)
     attributes.c_lflag &= ~(ICANON | ECHO | ISIG);
     attributes.c_iflag &= ~IXON;
     tcsetattr(STDIN_FILENO, TCSANOW, &attributes);
-    std::cout << "\x1b[?25lFor more info, use the --help option." << std::endl
-              << "Press the Escape key to exit" << std::endl
+    std::cout << "\x1b[?25lFor more info, use the \x1b[33m--help\x1b[39m option." << std::endl
+              << "Press the [\x1b[32mEscape\x1b[39m] key to exit" << std::endl
               << std::endl;
-    if (allowFocus)
-    {
-        std::cout << "    - Focus events are allowed." << std::endl;
-    }
-    if (allowMouse)
-    {
-        std::cout << "    - Mouse events are allowed." << std::endl;
-    }
-    if (allowFocus || allowMouse)
-    {
-        std::cout << std::endl;
-    }
+    std::cout << "    ~> Focus events are " << BOOLEAN_STATE(allowFocus) << "." << std::endl
+              << "    ~> Mouse events are " << BOOLEAN_STATE(allowMouse) << "." << std::endl
+              << std::endl;
     std::cout << "Waiting for a event to start...";
     while (true)
     {
         int character;
         bool isEscaping = (character = std::getchar()) == 27;
-        std::cout << "\x1b[2K\x1b[1GScanned Codes: " << character;
+        std::cout << "\x1b[2K\x1b[1G\x1b[1mScanned Codes:\x1b[22m " << character;
         fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
         int offset;
         for (offset = 0; (character = std::getchar()) != EOF; ++offset)
