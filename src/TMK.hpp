2867c12c58fc7661696bcbab3a452f4ae04658ec
Sherman Rofeman <skippyr.developer@gmail.com>
2024-06-04 18:29:06 -0300

    add Error docs

--- src/TMK.hpp
+++ src/TMK.hpp
@@ -5,17 +5,21 @@
 /**
  * @brief A powerful, basic and open-source C++ terminal manipulation library for Windows and Linux that includes
  * features the standard library lacks to handle terminal properties, colors,effects, arguments and event readings
- * using UTF-8 encoding.
+ * using UTF-8 encoding. It is heavily inspired by Java and the standard C# console library, thus having an object
+ * oriented philosophy for those who like a touch of complexity.
  */
 namespace TMK
 {
-    /** @brief Contains the available t erminal font effects. */
+    /** @brief Contains the available terminal font effects. */
     enum class Effect
     {
-        /** @brief Makes the text curly. */
+        /**
+         * @brief Makes the text curly. In order to be seen, the terminal might require the use of a font with italic
+         * style.
+         */
         Italic = 1 << 3,
-        /** @brief Draws a horizontal line below the text. */
-        Underline = 1 << 4,
+        /** @brief Draws a horizontal line crossing below the text. */
+        Underlined = 1 << 4,
         /** @brief Makes the text blink indefinitely. */
         Blinking = 1 << 5,
         /** @brief Swaps the foreground and background colors. */
@@ -150,15 +154,44 @@ namespace TMK
         static int GetFileNo();
     };
 
+    /** @brief Represents the standard terminal error stream. */
     class Error final
     {
     public:
         Error() = delete;
+        /**
+         * @brief Writes a formatted string to the error stream with a newline character appended at its end.
+         * @param format The format to be used. It uses the same specifiers as the printf function family.
+         * @param ... The arguments to be formatted.
+         * @returns 0 if successful or -1 otherwise.
+         */
         static int WriteLine(std::string format, ...);
+        /**
+         * @brief Writes a newline character to the error stream.
+         * @returns 0 if successful or -1 otherwise.
+         */
         static int WriteLine();
+        /**
+         * @brief Writes a formatted string to the error stream.
+         * @param format The format to be used. It uses the same specifiers as the printf function family.
+         * @param ... The arguments to be formatted.
+         * @returns 0 if successful or -1 otherwise.
+         */
         static int Write(std::string format, ...);
+        /**
+         * @brief Checks if the error stream is connected to an interactive terminal (TTY).
+         * @returns A boolean that states the stream is a TTY.
+         */
         static bool IsTTY();
+        /**
+         * @brief Gets the file descriptor associated with the error stream.
+         * @returns The file descriptor associated with the error stream.
+         */
         static std::FILE* GetFile();
+        /**
+         * @brief Gets the file descriptor number associated with the error stream.
+         * @returns The file descriptor number associated with the error stream.
+         */
         static int GetFileNo();
     };
 
