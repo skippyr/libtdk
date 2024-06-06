8601151df0d39dbe2cdaf0d939226ec384850765
Sherman Rofeman <skippyr.developer@gmail.com>
2024-06-06 13:50:39 -0300

    refactor Terminal class

--- src/TMK.cpp
+++ src/TMK.cpp
@@ -6,31 +6,30 @@
 #include <Windows.h>
 #include <io.h>
 #else
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <termios.h>
 #include <unistd.h>
 #endif
 
 #ifdef _WIN32
-#define TTY_CACHE(stream) (!!_isatty(stream::GetFileNo()) << stream::GetFileNo())
+#define TTY_CACHE(a_stream) (!!_isatty(a_stream::GetFileNumber()) << a_stream::GetFileNumber())
 #else
-#define TTY_CACHE(stream) (isatty(stream::GetFileNo()) << stream::GetFileNo())
+#define TTY_CACHE(a_stream) (isatty(a_stream::GetFileNumber()) << a_stream::GetFileNumber())
 #endif
-#define IS_TTY(stream) (g_cache & 1 << stream::GetFileNo())
+#define CACHE_HAS_BEEN_FILLED_FLAG (1 << 7)
 
 namespace TMK
 {
-    static void CacheTTYStatus();
-    static int WriteANSISequence(const char* format, ...);
+    char g_cache = 0;
 
-    static char g_cache = 0;
-
-    static void CacheTTYStatus()
+    class Setup
     {
-        if (!(g_cache & 1 << 7))
+    public:
+        static void InitEnvironment()
         {
-            g_cache |= TTY_CACHE(Input) | TTY_CACHE(Output) | TTY_CACHE(Error) | 1 << 7;
+            if (!(g_cache & CACHE_HAS_BEEN_FILLED_FLAG))
+            {
+                g_cache |= TTY_CACHE(Terminal::Input) | TTY_CACHE(Terminal::Output) | TTY_CACHE(Terminal::Error) |
+                           CACHE_HAS_BEEN_FILLED_FLAG;
+            }
 #ifdef _WIN32
             HANDLE handle;
             DWORD mode;
@@ -40,81 +39,19 @@ namespace TMK
                 SetConsoleMode(handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 #endif
         }
-    }
 
-    static int WriteANSISequence(const char* format, ...)
-    {
-        CacheTTYStatus();
-        if (!IS_TTY(Output) && !IS_TTY(Error))
-        {
-            return -1;
-        }
-        std::va_list arguments;
-        va_start(arguments, format);
-        int totalBytesWritten = std::vfprintf(IS_TTY(Output) ? stdout : stderr, format, arguments);
-        va_end(arguments);
-        return -(totalBytesWritten < 0);
-    }
+    private:
+        Setup() = delete;
+    };
 
-    bool Input::IsTTY()
-    {
-        CacheTTYStatus();
-        return IS_TTY(Input);
-    }
-
-    void Input::ClearBuffer()
-    {
-#ifdef _WIN32
-        FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
-#else
-        struct termios attributes;
-        int flags = fcntl(STDIN_FILENO, F_GETFL);
-        if (tcgetattr(STDIN_FILENO, &attributes))
-        {
-            return;
-        }
-        attributes.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &attributes);
-        fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
-        while (std::getchar() != EOF)
-        {
-        }
-        attributes.c_lflag |= ICANON | ECHO;
-        tcsetattr(STDIN_FILENO, TCSANOW, &attributes);
-        fcntl(STDIN_FILENO, F_SETFL, flags);
-#endif
-    }
-
-    std::FILE* Input::GetFile()
-    {
-        return stdin;
-    }
-
-    int Input::GetFileNo()
+    int Terminal::Input::GetFileNumber()
     {
         return 0;
     }
 
-    int Output::WriteLine(std::string format, ...)
+    int Terminal::Output::WriteLine(std::string format, ...)
     {
-        CacheTTYStatus();
-        std::va_list arguments;
-        va_start(arguments, format);
-        int totalBytesWritten = std::vprintf(format.c_str(), arguments);
-        std::putchar('\n');
-        va_end(arguments);
-        return -(totalBytesWritten < 0);
-    }
-
-    int Output::WriteLine()
-    {
-        CacheTTYStatus();
-        return -(std::putchar('\n') == EOF);
-    }
-
-    int Output::Write(std::string format, ...)
-    {
-        CacheTTYStatus();
+        Setup::InitEnvironment();
         std::va_list arguments;
         va_start(arguments, format);
         int totalBytesWritten = std::vprintf(format.c_str(), arguments);
@@ -122,248 +59,13 @@ namespace TMK
         return -(totalBytesWritten < 0);
     }
 
-    bool Output::IsTTY()
-    {
-        CacheTTYStatus();
-        return IS_TTY(Output);
-    }
-
-    void Output::FlushBuffer()
-    {
-        std::fflush(stdout);
-    }
-
-    std::FILE* Output::GetFile()
-    {
-        return stdout;
-    }
-
-    int Output::GetFileNo()
+    int Terminal::Output::GetFileNumber()
     {
         return 1;
     }
 
-    int Error::WriteLine(std::string format, ...)
-    {
-        CacheTTYStatus();
-        std::fflush(stdout);
-        std::va_list arguments;
-        va_start(arguments, format);
-        int totalBytesWritten = std::vfprintf(stderr, format.c_str(), arguments);
-        std::fputc('\n', stderr);
-        va_end(arguments);
-        return -(totalBytesWritten < 0);
-    }
-
-    int Error::WriteLine()
-    {
-        CacheTTYStatus();
-        std::fflush(stdout);
-        return -(std::fputc('\n', stderr) == EOF);
-    }
-
-    int Error::Write(std::string format, ...)
-    {
-        CacheTTYStatus();
-        std::fflush(stdout);
-        std::va_list arguments;
-        va_start(arguments, format);
-        int totalBytesWritten = std::vfprintf(stderr, format.c_str(), arguments);
-        va_end(arguments);
-        return -(totalBytesWritten < 0);
-    }
-
-    bool Error::IsTTY()
-    {
-        CacheTTYStatus();
-        return IS_TTY(Error);
-    }
-
-    std::FILE* Error::GetFile()
-    {
-        return stderr;
-    }
-
-    int Error::GetFileNo()
+    int Terminal::Error::GetFileNumber()
     {
         return 2;
-    }
-
-    Arguments::Arguments(int totalArguments, char** arguments)
-        : m_totalArguments(totalArguments), m_arguments(arguments)
-    {
-    }
-
-    Arguments::~Arguments()
-    {
-#ifdef _WIN32
-        for (int offset = 0; offset < m_totalArguments; ++offset)
-        {
-            delete[] m_arguments[offset];
-        }
-        delete[] m_arguments;
-#endif
-    }
-
-    int Arguments::GetTotalArguments() const
-    {
-        return m_totalArguments;
-    }
-
-    const char* Arguments::GetArgumentByOffset(std::size_t offset) const
-    {
-        return offset < m_totalArguments ? m_arguments[offset] : nullptr;
-    }
-
-    Dimensions::Dimensions() : m_totalColumns(0), m_totalRows(0)
-    {
-    }
-
-    Dimensions::Dimensions(unsigned short totalColumns, unsigned short totalRows)
-        : m_totalColumns(totalColumns), m_totalRows(totalRows)
-    {
-    }
-
-    unsigned short Dimensions::GetTotalColumns() const
-    {
-        return m_totalColumns;
-    }
-
-    unsigned short Dimensions::GetTotalRows() const
-    {
-        return m_totalRows;
-    }
-
-    Arguments Environment::GetArguments(int rawTotalArguments, char** rawArguments)
-    {
-#ifdef _WIN32
-        LPWSTR* argumentsUTF16 = CommandLineToArgvW(GetCommandLineW(), &rawTotalArguments);
-        char** items = new char*[rawTotalArguments];
-        for (int offset = 0; offset < rawTotalArguments; ++offset)
-        {
-            int size = WideCharToMultiByte(CP_UTF8, 0, argumentsUTF16[offset], -1, nullptr, 0, nullptr, nullptr);
-            items[offset] = new char[size];
-            WideCharToMultiByte(CP_UTF8, 0, argumentsUTF16[offset], -1, items[offset], size, nullptr, nullptr);
-        }
-        LocalFree(argumentsUTF16);
-        return Arguments(rawTotalArguments, items);
-#else
-        return Arguments(rawTotalArguments, rawArguments);
-#endif
-    }
-
-    void Font::SetWeight(Weight weight)
-    {
-        WriteANSISequence("\x1b[22;%dm", static_cast<int>(weight));
-    }
-
-    void Font::ResetWeight()
-    {
-        WriteANSISequence("\x1b[22m");
-    }
-
-    void Font::SetEffect(Effect effect)
-    {
-        SetEffect(static_cast<int>(effect));
-    }
-
-    void Font::SetEffect(int effect)
-    {
-        for (int code = 3; code < 10; ++code)
-        {
-            if (effect & 1 << code)
-            {
-                WriteANSISequence("\x1b[%dm", code);
-            }
-        }
-    }
-
-    void Font::ResetEffects()
-    {
-        for (int code = 23; code < 30; ++code)
-        {
-            if (code != 26)
-            {
-                WriteANSISequence("\x1b[%dm", code);
-            }
-        }
-    }
-
-    void Font::SetXColor(XColor color, Layer layer)
-    {
-        SetXColor(static_cast<int>(color), layer);
-    }
-
-    void Font::SetXColor(int color, Layer layer)
-    {
-        WriteANSISequence("\x1b[%d8;5;%dm", static_cast<int>(layer), color);
-    }
-
-    void Font::SetHexColor(int hex, Layer layer)
-    {
-        SetRGBColor(hex >> 16 & 0xff, hex >> 8 & 0xff, hex & 0xff, layer);
-    }
-
-    void Font::SetRGBColor(int red, int green, int blue, Layer layer)
-    {
-        WriteANSISequence("\x1b[%d8;2;%d;%d;%dm", static_cast<int>(layer), red, green, blue);
-    }
-
-    void Font::ResetColors()
-    {
-        WriteANSISequence("\x1b[39;49m");
-    }
-
-    int Window::GetDimensions(Dimensions& dimensions)
-    {
-#ifdef _WIN32
-        CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
-        if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo) &&
-            !GetConsoleScreenBufferInfo(GetStdHandle(STD_ERROR_HANDLE), &bufferInfo))
-        {
-            return -1;
-        }
-        dimensions = Dimensions(bufferInfo.srWindow.Right - bufferInfo.srWindow.Left + 1,
-                                bufferInfo.srWindow.Bottom - bufferInfo.srWindow.Top + 1);
-#else
-        struct winsize size;
-        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) && ioctl(STDIN_FILENO, TIOCGWINSZ, &size) &&
-            ioctl(STDERR_FILENO, TIOCGWINSZ, &size))
-        {
-            return -1;
-        }
-        dimensions = Dimensions(size.ws_col, size.ws_row);
-#endif
-        return 0;
-    }
-
-    void Window::SetTitle(std::string title)
-    {
-        WriteANSISequence("\x1b]0;%s\7", title.c_str());
-    }
-
-    void Window::OpenAlternateBuffer()
-    {
-        WriteANSISequence("\x1b[?1049h\x1b[2J\x1b[1;1H");
-    }
-
-    void Window::CloseAlternateBuffer()
-    {
-        WriteANSISequence("\x1b[?1049l");
-    }
-
-    void Window::Clear()
-    {
-        WriteANSISequence("\x1b[2J\x1b[1;1H");
-    }
-
-    void Bell::Ring()
-    {
-        WriteANSISequence("\7");
-    }
-
-    int operator|(Effect effect0, Effect effect1)
-    {
-        return static_cast<int>(effect0) | static_cast<int>(effect1);
     }
 }
--- src/TMK.hpp
+++ src/TMK.hpp
@@ -1,381 +1,41 @@
 #pragma once
 
-#include <string>
+#include <iostream>
 
-/**
- * @brief A powerful, basic and open-source C++ terminal manipulation library for Windows and Linux that includes
- * features the standard library lacks to handle terminal properties, colors,effects, arguments and event readings
- * using UTF-8 encoding. It is heavily inspired by Java and the standard C# console library, thus having an object
- * oriented philosophy for those who like a touch of complexity.
- */
 namespace TMK
 {
-    /** @brief Contains the available terminal font effects. */
-    enum class Effect
-    {
-        /**
-         * @brief Makes the text curly. In order to be seen, the terminal might require the use of a font with italic
-         * style.
-         */
-        Italic = 1 << 3,
-        /** @brief Draws a horizontal line crossing below the text. */
-        Underlined = 1 << 4,
-        /** @brief Makes the text blink indefinitely. */
-        Blinking = 1 << 5,
-        /** @brief Swaps the foreground and background colors. */
-        Negative = 1 << 7,
-        /** @brief Makes the text hard to see or invisible. */
-        Hidden = 1 << 8,
-        /** @brief Draws a horizontal line crossing through the middle of the text. */
-        CrossedOut = 1 << 9
-    };
-
-    /** @brief Contains the available layers where a terminal color can be set on the font. */
-    enum class Layer
-    {
-        /** @brief Affects the color of the letters. */
-        Foreground = 3,
-        /** @brief Affects the background color of the letters. */
-        Background
-    };
-
-    /** @brief Contains the available terminal font weights. */
-    enum class Weight
-    {
-        /** @brief Usually rendered as bold and/or with bright color. */
-        Bold = 1,
-        /** @brief Usually rendered with faint color. */
-        Light
-    };
-
-    /** @brief Contains the ANSI codes of the first 16 colors of the XTerm palette. */
-    enum class XColor
-    {
-        /** @brief The regular variant of the black color. */
-        Black,
-        /** @brief The regular variant of the red color. */
-        Red,
-        /** @brief The regular variant of the green color. */
-        Green,
-        /** @brief The regular variant of the yellow color. */
-        Yellow,
-        /** @brief The regular variant of the blue color. */
-        Blue,
-        /** @brief The regular variant of the magenta color. */
-        Magenta,
-        /** @brief The regular variant of the cyan color. */
-        Cyan,
-        /** @brief The regular variant of the white color. */
-        White,
-        /** @brief The light variant of the black color. */
-        LightBlack,
-        /** @brief The light variant of the red color. */
-        LightRed,
-        /** @brief The light variant of the green color. */
-        LightGreen,
-        /** @brief The light variant of the yellow color. */
-        LightYellow,
-        /** @brief The light variant of the blue color. */
-        LightBlue,
-        /** @brief The light variant of the magenta color. */
-        LightMagenta,
-        /** @brief The light variant of the cyan color. */
-        LightCyan,
-        /** @brief The light variant of the white color. */
-        LightWhite
-    };
-
-    /** @brief Represents the standard terminal input stream. */
-    class Input final
+    class Terminal
     {
     public:
-        /**
-         * @brief Checks if the input stream is connected to an interactive terminal (TTY).
-         * @returns A boolean that states the stream is a TTY.
-         */
-        static bool IsTTY();
-        /** @brief Clears the input buffer. */
-        static void ClearBuffer();
-        /**
-         * @brief Gets the file descriptor associated with the input stream.
-         * @returns The file descriptor associated with the stream.
-         */
-        static std::FILE* GetFile();
-        /**
-         * @brief Gets the file descriptor number associated with the input stream.
-         * @returns The file descriptor number associated with the stream.
-         */
-        static int GetFileNo();
+        class Input
+        {
+        public:
+            static int GetFileNumber();
+
+        private:
+            Input() = delete;
+        };
+
+        class Output
+        {
+        public:
+            static int WriteLine(std::string format, ...);
+            static int GetFileNumber();
+
+        private:
+            Output() = delete;
+        };
+
+        class Error
+        {
+        public:
+            static int GetFileNumber();
+
+        private:
+            Error() = delete;
+        };
 
     private:
-        Input() = delete;
+        Terminal() = delete;
     };
-
-    /** @brief Represents the standard terminal output stream. */
-    class Output final
-    {
-    public:
-        /**
-         * @brief Writes a formatted string to the output stream with a newline character appended at its end.
-         * @param format The format to be used. It uses the same specifiers as the printf function family.
-         * @param ... The arguments to be formatted.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int WriteLine(std::string format, ...);
-        /**
-         * @brief Writes a newline character to the output stream.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int WriteLine();
-        /**
-         * @brief Writes a formatted string to the output stream.
-         * @param format The format to be used. It uses the same specifiers as the printf function family.
-         * @param ... The arguments to be formatted.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int Write(std::string format, ...);
-        /**
-         * @brief Checks if the output stream is connected to an interactive terminal (TTY).
-         * @returns A boolean that states the stream is a TTY.
-         */
-        static bool IsTTY();
-        /** @brief Flushes the output buffer. */
-        static void FlushBuffer();
-        /**
-         * @brief Gets the file descriptor associated with the output stream.
-         * @returns The file descriptor associated with the stream.
-         */
-        static std::FILE* GetFile();
-        /**
-         * @brief Gets the file descriptor number associated with the output stream.
-         * @returns The file descriptor number associated with the stream.
-         */
-        static int GetFileNo();
-
-    private:
-        Output() = delete;
-    };
-
-    /** @brief Represents the standard terminal error stream. */
-    class Error final
-    {
-    public:
-        /**
-         * @brief Writes a formatted string to the error stream with a newline character appended at its end.
-         * @param format The format to be used. It uses the same specifiers as the printf function family.
-         * @param ... The arguments to be formatted.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int WriteLine(std::string format, ...);
-        /**
-         * @brief Writes a newline character to the error stream.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int WriteLine();
-        /**
-         * @brief Writes a formatted string to the error stream.
-         * @param format The format to be used. It uses the same specifiers as the printf function family.
-         * @param ... The arguments to be formatted.
-         * @returns 0 if successful or -1 otherwise.
-         */
-        static int Write(std::string format, ...);
-        /**
-         * @brief Checks if the error stream is connected to an interactive terminal (TTY).
-         * @returns A boolean that states the stream is a TTY.
-         */
-        static bool IsTTY();
-        /**
-         * @brief Gets the file descriptor associated with the error stream.
-         * @returns The file descriptor associated with the stream.
-         */
-        static std::FILE* GetFile();
-        /**
-         * @brief Gets the file descriptor number associated with the error stream.
-         * @returns The file descriptor number associated with the stream.
-         */
-        static int GetFileNo();
-
-    private:
-        Error() = delete;
-    };
-
-    /** @brief Represents the terminal command-line arguments. */
-    class Arguments final
-    {
-    public:
-        /**
-         * @brief Creates an instance of the Arguments class.
-         * @param totalArguments The total of arguments.
-         * @param arguments The arguments.
-         */
-        Arguments(int totalArguments, char** arguments);
-        /** @brief Destroys an instance of the Arguments class. */
-        ~Arguments();
-        /**
-         * @brief Gets the total arguments.
-         * @returns The total arguments.
-         */
-        int GetTotalArguments() const;
-        /**
-         * @brief Gets an argument by its offset.
-         * @param offset The offset of the argument.
-         * @returns The argument if successful or nullptr otherwise.
-         */
-        const char* GetArgumentByOffset(std::size_t offset) const;
-
-    private:
-        /** @brief The total arguments. */
-        int m_totalArguments;
-        /** @brief The arguments. */
-        char** m_arguments;
-    };
-
-    /** @brief Represents dimensions inside of the terminal window. */
-    class Dimensions final
-    {
-    public:
-        /** @brief Creates a new instance of the Dimensions class. */
-        Dimensions();
-        /**
-         * @brief Creates a new instance of the Dimensions class.
-         * @param totalColumns The total columns of the dimensions.
-         * @param totalRows The total rows of the dimensions.
-         */
-        Dimensions(unsigned short totalColumns, unsigned short totalRows);
-        /**
-         * @brief Gets the total columns of the dimensions.
-         * @returns The total columns of the dimensions.
-         */
-        unsigned short GetTotalColumns() const;
-        /**
-         * @brief Gets the total rows of the dimensions.
-         * @returns The total rows of the dimensions.
-         */
-        unsigned short GetTotalRows() const;
-
-    private:
-        /** @brief The total columns of the dimensions. */
-        unsigned short m_totalColumns;
-        /** @brief The total rows of the dimensions. */
-        unsigned short m_totalRows;
-    };
-
-    /** @brief Represents the environment the terminal is running on. */
-    class Environment final
-    {
-    public:
-        /**
-         * @brief Gets and treats the command-line arguments associated with the terminal process.
-         * @param rawTotalArguments The total arguments given as parameter of the main function.
-         * @param rawArguments The arguments given as parameter of the main function.
-         * @returns The treated arguments associated with the terminal process.
-         */
-        static Arguments GetArguments(int rawTotalArguments, char** rawArguments);
-
-    private:
-        Environment() = delete;
-    };
-
-    /** @brief Represents the terminal font. */
-    class Font final
-    {
-    public:
-        /**
-         * @brief Sets the font weight.
-         * @param weight The weight to be set.
-         */
-        static void SetWeight(Weight weight);
-        /** @brief Sets the font weight to default. */
-        static void ResetWeight();
-        /**
-         * @brief Sets a font effect.
-         * @param effect The effect to be set.
-         * @param isToEnable A boolean that state the effects should be enabled or disabled.
-         */
-        static void SetEffect(Effect effect);
-        /**
-         * @brief Sets one or multiple font effects.
-         * @param effect A bitmask containing the effects to be set. It must be composed by enumerators from the Effect
-         * enum class.
-         * @param isToEnable A boolean that state the effects should be enabled or disabled.
-         */
-        static void SetEffect(int effect);
-        /** @brief Disables all font effects. */
-        static void ResetEffects();
-        /**
-         * @brief Sets an XTerm font color.
-         * @param color The color to be set.
-         * @param layer The layer where the color should be applied on.
-         */
-        static void SetXColor(XColor color, Layer layer);
-        /**
-         * @brief Sets an XTerm font color.
-         * @param color The color to be set. It must be a value in range from 0 to 255.
-         * @param layer The layer where the color should be applied on.
-         */
-        static void SetXColor(int color, Layer layer);
-        /**
-         * @brief Sets a hex font color.
-         * @param hex The color to be set. It must be a value in range from 0 to 0xffffff.
-         * @param layer The layer where the color should be applied on.
-         */
-        static void SetHexColor(int hex, Layer layer);
-        /**
-         * @brief Sets an RGB font color.
-         * @param red The red component of the color. It must be a value in range from 0 to 255.
-         * @param green The green component of the color. It must be a value in range from 0 to 255.
-         * @param blue The blue component of the color. It must be a value in range from 0 to 255.
-         * @param layer The layer where the color should be applied on.
-         */
-        static void SetRGBColor(int red, int green, int blue, Layer layer);
-        /** @brief Sets the font foreground and background colors to default. */
-        static void ResetColors();
-
-    private:
-        Font() = delete;
-    };
-
-    /** @brief Represents the terminal window. */
-    class Window final
-    {
-    public:
-        /**
-         * @brief Gets the terminal window dimensions.
-         * @param dimensions The address where the dimensions information will be put into.
-         */
-        static int GetDimensions(Dimensions& dimensions);
-        /**
-         * @brief Sets the terminal window title.
-         * @param title The title to be set.
-         */
-        static void SetTitle(std::string title);
-        /** @brief Opens the terminal alternate window buffer. */
-        static void OpenAlternateBuffer();
-        /** @brief Closes the terminal alternate window buffer. */
-        static void CloseAlternateBuffer();
-        /** @brief Clears the terminal window. */
-        static void Clear();
-
-    private:
-        Window() = delete;
-    };
-
-    /** @brief Represents the terminal bell. */
-    class Bell final
-    {
-    public:
-        /** @brief Rings the terminal bell. */
-        static void Ring();
-
-    private:
-        Bell() = delete;
-    };
-
-    /**
-     * @brief Concatenates two font effects together in order to create a bitmask containing both.
-     * @param effect0 The first effect.
-     * @param effect1 The second effect.
-     * @returns A bitmask containing both effects.
-     */
-    int operator|(Effect effect0, Effect effect1);
 }
